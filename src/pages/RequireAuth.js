ce36b711db03e6b321e071b1197f2ebe1bd25d3e
abhishekchauhan9 <abhishek.chauhan@avista.com>
2025-08-20 13:11:03 +0530

    remove unnecessary function

--- src/pages/RequireAuth.js
+++ src/pages/RequireAuth.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
-export default function RequireAuth({ children }){
+export default function RequireAuth({ children }) {
   const { user } = useAuth();
-  const location = useLocation();
-  if(!user){
-    return <Navigate to="/login" replace state={{ from: location }} />;
+  if (!user) {
+    return <Navigate to="/login" />;
   }
   return children;
 }
